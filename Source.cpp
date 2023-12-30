dd11b3dabf60a2a44e2f1a29d2cfe43a2c49ba3d
maeeirdX <149462595+maeeirdX@users.noreply.github.com>
2023-12-30 16:02:48 +0300

    8

--- Source.cpp
+++ Source.cpp
@@ -1,248 +1,72 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <iomanip>
 #include<Windows.h>
-
-struct Student {
-    std::string name;
-    std::vector<int> grades;
-};
-
-std::vector<Student> students;
-
-// Функция для добавления нового ученика
-void addStudent() {
-    Student student;
-    std::cout << "Введите имя ученика: ";
-    std::getline(std::cin, student.name);
-    students.push_back(student);
-}
-
-// Функция для добавления оценок для ученика
-void addGrades() {
-    std::string name;
-    std::cout << "Введите имя ученика: ";
-    std::getline(std::cin, name);
-
-    for (auto& student : students) {
-        if (student.name == name) {
-            int grade;
-            std::cout << "Введите оценку(-1 для завершения): ";
-            while (std::cin >> grade && grade != -1) {
-                student.grades.push_back(grade);
-                std::cout << "Введите оценку(-1 для завершения): ";
-            }
-            break;
-        }
-    }
-    std::cin.ignore();
-}
-
-// Функция для удаления ученика или оценок
-void removeStudentOrGrades() {
-    std::string name;
-    std::cout << "Введите имя ученика: ";
-    std::getline(std::cin, name);
-
-    for (auto it = students.begin(); it != students.end(); ) {
-        if (it->name == name) {
-            int choice;
-            std::cout << "Выберите: (1) Удалить учащегося (2) Удалить оценки: ";
-            std::cin >> choice;
-            if (choice == 1) {
-                it = students.erase(it);
-            }
-            else if (choice == 2) {
-                it->grades.clear();
-                ++it;
-            }
-            else {
-                ++it;
-            }
-        }
-        else {
-            ++it;
-        }
-    }
-    std::cin.ignore();
-}
-
-// Функция для изменения оценки у ученика
-void changeGrade() {
-    std::string name;
-    std::cout << "Введите имя ученика: ";
-    std::getline(std::cin, name);
-
-    for (auto& student : students) {
-        if (student.name == name) {
-            int index;
-            std::cout << "Введите оценку: ";
-            std::cin >> index;
-
-            if (index >= 0 && index < student.grades.size()) {
-                int newGrade;
-                std::cout << "Введите новую оценку: ";
-                std::cin >> newGrade;
-                student.grades[index] = newGrade;
-            }
-            break;
-        }
-    }
-    std::cin.ignore();
-}
-
-// Функция для расчета итоговой оценки у ученика или всех учеников
-void calculateTotalGrade() {
-    std::string name;
-    std::cout << "Введите имя ученика: ";
-    std::getline(std::cin, name);
-
-    if (name == "all") {
-        for (const auto& student : students) {
-            int total = 0;
-            for (int grade : student.grades) {
-                total += grade;
-            }
-
-            double average = static_cast<double>(total) / student.grades.size();
-            std::cout << "Студенты: " << student.name << ", Средний балл: " << std::fixed << std::setprecision(2) << average << std::endl;
-        }
-    }
-    else {
-        for (const auto& student : students) {
-            if (student.name == name) {
-                int total = 0;
-                for (int grade : student.grades) {
-                    total += grade;
-                }
-
-                double average = static_cast<double>(total) / student.grades.size();
-                std::cout << "Студенты: " << student.name << ", Средний балл: " << std::fixed << std::setprecision(2) << average << std::endl;
-                break;
-            }
-        }
-    }
-}
-
-// Функция для сохранения изменений в файл
-void saveChanges(const std::string& filename) {
-    std::ofstream file(filename);
-
-    if (file) {
-        for (const auto& student : students) {
-            file << student.name << " ";
-            for (int grade : student.grades) {
-                file << grade << " ";
-            }
-            file << std::endl;
-        }
-        std::cout << "Изменения сохранены!" << std::endl;
-    }
-    else {
-        std::cout << "Ошибка сохранения изменений." << std::endl;
-    }
-
-    file.close();
-}
-
-// Функция для импорта из файла
-void importFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-
-    if (file) {
-        students.clear();
-        std::string line;
-        while (std::getline(file, line)) {
-            std::istringstream iss(line);
-            Student student;
-            iss >> student.name;
-            int grade;
-            while (iss >> grade) {
-                student.grades.push_back(grade);
-            }
-            students.push_back(student);
-        }
-    }
-    else {
-        std::cout << "Ошибка импорта файла." << std::endl;
-    }
-
-    file.close();
-}
-
-// Функция для экспорта в файл
-void exportToFile(const std::string& filename) {
-    std::ofstream file(filename);
-
-    if (file) {
-        for (const auto& student : students) {
-            file << student.name << " ";
-            for (int grade : student.grades) {
-                file << grade << " ";
-            }
-            file << std::endl;
-        }
-    }
-    else {
-        std::cout << "Ошибка экспорта" << std::endl;
-    }
-
-    file.close();
-}
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::string filename = "journal.txt";
 
-    importFromFile(filename);
+    std::string filename = "answers.txt";
 
-    int choice;
-    do {
-        std::cout << "Выберите функцию:" << std::endl;
-        std::cout << "1. Добавить ученика" << std::endl;
-        std::cout << "2. Добавить оценки" << std::endl;
-        std::cout << "3. Удаление учащегося или оценки" << std::endl;
-        std::cout << "4. Изменение оценки" << std::endl;
-        std::cout << "5. Рассчитать среднюю оценку учащегося" << std::endl;
-        std::cout << "6. Сохранить изменения в файл" << std::endl;
-        std::cout << "0. Выйти" << std::endl;
-        std::cout << "Выберите функцию: ";
-        std::cin >> choice;
-        std::cin.ignore();
+    // Считываем ответы пользователя и записываем в файл
+    std::ofstream file;
+    file.open(filename, std::ios::binary);
 
-        switch (choice) {
-        case 1:
-            addStudent();
-            break;
-        case 2:
-            addGrades();
-            break;
-        case 3:
-            removeStudentOrGrades();
-            break;
-        case 4:
-            changeGrade();
-            break;
-        case 5:
-            calculateTotalGrade();
-            break;
-        case 6:
-            saveChanges(filename);
-            break;
-        case 0:
-            exportToFile(filename);
-            break;
-        default:
-            std::cout << "Неверный выбор. Попробуйте ещё раз(" << std::endl;
-            break;
+    if (!file.is_open()) {
+        std::cout << "Ошибка при открытии файла" << std::endl;
+        return 1;
+    }
+
+    std::string questions[8] = {
+        "Вам нравится программирование?",
+        "Вы знакомы с языком программирования C++?",
+        "Вам нравится C++?",
+        "Вы готовы учиться и развиваться в области программирования?",
+        "Вам нравится решать сложные задачи?",
+        "Вы эффективно управляете своим временем?",
+        "Вы готовы работать в команде разработчиков?",
+        "Вам нравится создавать новые программы и приложения?"
+    };
+
+    std::string answers[8];
+
+    for (int i = 0; i < 8; i++) {
+        std::cout << questions[i] << " (да/нет): ";
+        std::cin >> answers[i];
+
+        answers[i] = (answers[i] == "да" || answers[i] == "Да" || answers[i] == "ДА") ? "да" : "нет";
+
+        file.write(answers[i].c_str(), 1);
+    }
+
+    file.close();
+    std::cout << "Ответы записаны в файл " << filename << std::endl;
+
+    // Пользователь может изменить свои ответы после окончания работы программы
+    std::cout << "Вы хотите изменить свои ответы? (да/нет): ";
+    std::string changeAnswer;
+    std::cin >> changeAnswer;
+
+    if (changeAnswer == "да" || changeAnswer == "Да" || changeAnswer == "ДА") {
+        file.open(filename, std::ios::out | std::ios::trunc);
+
+        if (!file.is_open()) {
+            std::cout << "Ошибка при открытии файла" << std::endl;
+            return 1;
         }
 
-        std::cout << std::endl;
-    } while (choice != 0);
+        for (int i = 0; i < 8; i++) {
+            std::cout << questions[i] << " (да/нет): ";
+            std::cin >> answers[i];
+
+            answers[i] = (answers[i] == "да" || answers[i] == "Да" || answers[i] == "ДА") ? "да" : "нет";
+
+            file.write(answers[i].c_str(), 1);
+        }
+
+        file.close();
+        std::cout << "Ваши новые ответы записаны в файл " << filename << std::endl;
+    }
 
     return 0;
 }
