33d821ccedf49a1e88ef8fe8c9db09f5b301a540
maeeirdX <149462595+maeeirdX@users.noreply.github.com>
2023-12-30 16:27:50 +0300

    12

--- Source.cpp
+++ Source.cpp
@@ -1,248 +1,75 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <algorithm>
-#include <sstream>
-#include <string>
-#include <limits>
-#include <iterator>
-
-using namespace std;
-
-struct Book {
-    string title;
-    string author;
-    string publisher;
-    int year;
-    double price;
-};
-
-void printBook(const Book& book, int number) {
-    cout << number << ". " << book.title << "\t" << book.author << "\t" << book.publisher << "\t" << book.year << "\t" << book.price << endl;
-}
-
-void printBooks(const vector<Book>& books) {
-    for (size_t i = 0; i < books.size(); ++i) {
-        printBook(books[i], i + 1);
-    }
-}
-
-void printBooksWithNumbers(const vector<Book>& books) {
-    for (size_t i = 0; i < books.size(); ++i) {
-        cout << i + 1 << ". ";
-        printBook(books[i], i + 1);
-    }
-}
-
-void addBook(ofstream& file, vector<Book>& books, const Book& book) {
-    books.push_back(book);
-    file << book.title << "\t" << book.author << "\t" << book.publisher << "\t" << book.year << "\t" << book.price << endl;
-}
-
-void removeBook(const string& filePath, vector<Book>& books) {
-    ifstream inputFile(filePath);
-    ofstream tempFile("temp.txt", ios::out);
-
-    vector<string> numbersToRemove;
-    cout << "Введите номера книг для удаления (через пробел): ";
-    string input;
-    getline(cin, input);
-    istringstream iss(input);
-    copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(numbersToRemove));
-
-    bool bookFound = false;
-
-    for (size_t i = 0; i < numbersToRemove.size(); ++i) {
-        int bookIndex = stoi(numbersToRemove[i]) - 1;
-        if (bookIndex >= 0 && static_cast<size_t>(bookIndex) < books.size()) {
-            books.erase(books.begin() + bookIndex);
-            bookFound = true;
-        }
-    }
-
-    if (!bookFound) {
-        cout << "Книги не найдены." << endl;
-    }
-
-    for (const Book& book : books) {
-        tempFile << book.title << "\t" << book.author << "\t" << book.publisher << "\t" << book.year << "\t" << book.price << endl;
-    }
-
-    tempFile.close();
-    inputFile.close();
-
-    remove(filePath.c_str());
-    rename("temp.txt", filePath.c_str());
-
-    if (bookFound) {
-        cout << "Книги успешно удалены." << endl;
-    }
-}
-
-void addBooksFromFile(ofstream& file, vector<Book>& books, const string& filePath) {
-    ifstream inputFile(filePath);
-    if (!inputFile) {
-        cerr << "Ошибка открытия файла для чтения." << endl;
-        return;
-    }
-
-    string line;
-    int lineNum = 0;
-    while (getline(inputFile, line)) {
-        ++lineNum;
-        stringstream ss(line);
-
-        Book book;
-        if (getline(ss, book.title, '\t') && getline(ss, book.author, '\t') &&
-            getline(ss, book.publisher, '\t') && (ss >> book.year >> book.price)) {
-            addBook(file, books, book);
-        }
-        else {
-            cerr << "Ошибка в строке " << lineNum << ": некорректный формат данных." << endl;
-        }
-    }
-
-    inputFile.close();
-    cout << "Книги успешно добавлены из другого файла." << endl;
-}
-
-vector<Book> readBooksFromFile(ifstream& file) {
-    vector<Book> books;
-    Book book;
-
-    string line;
-    while (getline(file, line)) {
-        stringstream ss(line);
-
-        if (getline(ss, book.title, '\t') && getline(ss, book.author, '\t') &&
-            getline(ss, book.publisher, '\t') && (ss >> book.year >> book.price)) {
-            books.push_back(book);
-        }
-    }
-
-    return books;
-}
-
-void clearInputBuffer() {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
+#include <queue>
+#include<Windows.h>
 
 int main() {
-    string filePath;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    // Запрашиваем размеры матрицы
+    int n, m;
+    std::cout << "Введите размеры матрицы (n и m): ";
+    std::cin >> n >> m;
 
-    cout << "Укажите полный путь к файлу: ";
-    getline(cin, filePath);
+    // Создаем матрицу
+    std::vector<std::vector<int>> matrix(n, std::vector<int>(m));
 
-    ifstream inputFile(filePath);
-
-    while (!inputFile.is_open()) {
-        cout << "Ошибка открытия файла, попробуйте еще раз: ";
-        getline(cin, filePath);
-        inputFile.open(filePath);
-    }
-
-    vector<Book> books = readBooksFromFile(inputFile);
-    inputFile.close();
-
-    while (true) {
-        cout << "Опции:" << endl;
-        cout << "1. Вывести книги" << endl;
-        cout << "2. Вывести книги из диапазона цен" << endl;
-        cout << "3. Добавить новую книгу" << endl;
-        cout << "4. Удалить книгу" << endl;
-        cout << "5. Добавить книги из другого файла" << endl;
-        cout << "6. Выйти" << endl;
-
-        string option;
-        cout << "Введите свой вариант: ";
-        getline(cin, option);
-
-        if (option == "1") {
-            cout << "Книги, находящиеся в файле:" << endl;
-            printBooks(books);
-        }
-        else if (option == "2") {
-            cout << "Укажите диапазон цен (мин. макс.): ";
-            double minPrice, maxPrice;
-
-            // Проверка на корректный ввод минимальной цены
-            while (!(cin >> minPrice) || minPrice <= 0) {
-                cout << "Ошибка ввода. Введите корректное положительное значение для минимальной цены, не начинающееся с нуля: ";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-
-            // Проверка на корректный ввод максимальной цены
-            while (!(cin >> maxPrice) || maxPrice <= 0 || maxPrice < minPrice) {
-                cout << "Ошибка ввода. Введите корректное положительное значение для максимальной цены (больше или равное минимальной): ";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-
-            vector<Book> filteredBooks;
-            copy_if(books.begin(), books.end(), back_inserter(filteredBooks), [minPrice, maxPrice](const Book& book) {
-                return book.price >= minPrice && book.price <= maxPrice;
-                });
-
-            cout << "Отфильтрованные книги:" << endl;
-            printBooks(filteredBooks);
-
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        }
-        else if (option == "3") {
-            Book newBook;
-            cout << "Введите название: ";
-            getline(cin, newBook.title);
-            cout << "Введите автора: ";
-            getline(cin, newBook.author);
-            cout << "Введите издательство: ";
-            getline(cin, newBook.publisher);
-
-            cout << "Введите год: ";
-            while (!(cin >> newBook.year) || newBook.year < 1) {
-                cout << "Ошибка ввода года. Попробуйте снова:" << endl;
-                clearInputBuffer();
-            }
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Игнорируем символ новой строки
-
-            cout << "Введите цену: ";
-            while (!(cin >> newBook.price) || newBook.year < 0) {
-                cerr << "Ошибка ввода цены. Попробуйте снова." << endl;
-                clearInputBuffer();
-            }
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Игнорируем символ новой строки
-
-            ofstream outputFile(filePath, ios::app);
-            addBook(outputFile, books, newBook);
-            outputFile.close();
-
-            cout << "Новая книга успешно добавлена." << endl;
-
-        }
-        else if (option == "4") {
-            printBooks(books);
-            removeBook(filePath, books);
-
-        }
-        else if (option == "5") {
-            string otherFilePath;
-            cout << "Укажите путь к другому файлу: ";
-            getline(cin, otherFilePath);
-            ofstream outputFile(filePath, ios::app);
-            addBooksFromFile(outputFile, books, otherFilePath);
-            outputFile.close();
-
-        }
-        else if (option == "6") {
-            cout << "Выход...." << endl;
-            break;
-        }
-        else {
-            cerr << "Недопустимый вариант." << endl;
+    // Заполняем матрицу случайными цветами
+    std::vector<int> colors = { 1, 2, 3, 4, 5, 6, 7 };
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = colors[rand() % 7];
         }
     }
+
+    // Вспомогательный массив для отслеживания пройденных ячеек
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(m, false));
+
+    // Массив для хранения размеров областей
+    std::vector<int> areas;
+
+    // Обходим каждую ячейку матрицы
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            // Если ячейка не посещена, начинаем новую область
+            if (!visited[i][j]) {
+                int color = matrix[i][j];
+                int area = 0;
+                // Используем очередь для поиска смежных ячеек одного цвета
+                std::queue<std::pair<int, int>> q;
+                q.push({ i, j });
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    // Проверяем смежные ячейки
+                    if (x >= 0 && x < n && y >= 0 && y < m && !visited[x][y] && matrix[x][y] == color) {
+                        visited[x][y] = true;
+                        area++;
+                        // Добавляем смежные ячейки в очередь
+                        q.push({ x - 1, y });
+                        q.push({ x + 1, y });
+                        q.push({ x, y - 1 });
+                        q.push({ x, y + 1 });
+                    }
+                }
+                // Добавляем размер области в массив
+                areas.push_back(area);
+            }
+        }
+    }
+
+    // Находим максимальную область
+    int maxArea = 0;
+    for (int i = 0; i < areas.size(); i++) {
+        if (areas[i] > maxArea) {
+            maxArea = areas[i];
+        }
+    }
+
+    // Выводим результат
+    std::cout << "Наибольшая область одинакового цвета: " << maxArea << std::endl;
 
     return 0;
 }
